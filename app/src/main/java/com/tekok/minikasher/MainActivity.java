d99454279766764e30fcf3800afb34dabe50af21
Omer_Baraja <newprogramming1@gmail.com>
2020-10-24 12:06:17 +0300

    start2

--- app/src/main/java/com/tekok/minikasher/MainActivity.java
+++ app/src/main/java/com/tekok/minikasher/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // اهلا ايمن ^_^
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
